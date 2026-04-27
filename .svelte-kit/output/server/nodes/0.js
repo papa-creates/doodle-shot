a30888881e39678d005a0d0fcdd07ed0b0c21955
papa-creates <233835846+papa-creates@users.noreply.github.com>
2026-04-27 19:49:54 -0400

    chore: stop tracking generated artifacts

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.D7fGoNei.js","_app/immutable/chunks/iYE05zWP.js","_app/immutable/chunks/mEjUREfi.js","_app/immutable/chunks/pUgZARa3.js"];
-export const stylesheets = [];
-export const fonts = [];
